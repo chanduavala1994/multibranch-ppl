26abe25228ebaf83c5e1464740f84e1058d45ffd
Chandu Avala <ChanduAvala1994@gmail.com>
2024-04-14 22:40:15 +0530

    first commit

---
+++ mbp.groovy
@@ -0,0 +1,50 @@
+
+pipeline{
+    agent any
+
+    stages{
+        stage("Code Checkout"){
+            when {
+                branch "develop"
+            }
+            steps{
+                git url:"https://github.com/javahometech/leads-mbp", branch: "main"
+            }
+        }
+
+        stage("Maven Build"){
+            when {
+                branch "develop"
+            }
+            steps{
+                echo "maven build...."
+            }
+        }
+        stage("Dev Deploy"){
+            when {
+                branch "develop"
+            }
+            steps{
+                echo "Deploy to development environment"
+            }
+        }
+        stage("Test Deploy"){
+            when {
+                branch "test"
+            }
+            steps{
+                echo "Deploy to test environment"
+            }
+        }
+
+        stage("Prod Deploy"){
+            when {
+                branch "main"
+            }
+            steps{
+                echo "Deploy to prod environment"
+            }
+        }
+        
+    }
+}
